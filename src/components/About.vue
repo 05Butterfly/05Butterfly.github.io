949ce5f042ee702c959a480b107a2cde711f99a7
05Butterfly <rcn02468@gmail.com>
2025-03-13 13:00:23 +0100

    update on about

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,19 +2,47 @@
     <header>
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="0"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                  <img class="d-block w-100" src="../assets/imgs/slides/" alt="First slide">
+                  <img class="d-block w-25" src="../assets/imgs/slides/slide01.jpeg" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                  <img class="d-block w-100" src="..." alt="Second slide">
+                  <img class="d-block w-25" src="../assets/imgs/slides/slide02.jpeg" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                  <img class="d-block w-100" src="..." alt="Third slide">
+                  <img class="d-block w-25" src="../assets/imgs/slides/slide03.jpeg" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-25" src="../assets/imgs/slides/slide04.jpeg" alt="Fourth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-25" src="../assets/imgs/slides/slide05.jpeg" alt="Fifth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-25" src="../assets/imgs/slides/slide06.jpeg" alt="Sixth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-25" src="../assets/imgs/slides/slide07.jpeg" alt="Seventh slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-25" src="../assets/imgs/slides/slide08.jpeg" alt="Eighth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-25" src="../assets/imgs/slides/slide09.jpeg" alt="Nineth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-25" src="../assets/imgs/slides/slide10.jpeg" alt="Tenth slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
