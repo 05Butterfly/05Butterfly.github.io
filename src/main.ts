e0511da1d771723eec3c8132058df91463d9ccc0
05Butterfly <rcn02468@gmail.com>
2025-03-13 17:02:45 +0100

    adding router

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router.js'
 
 import i18n from './i18n.ts'
 
 const app = createApp(App)
+app.use(router);
 app.use(i18n)
 
 app.mount('#app')
